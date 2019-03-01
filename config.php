41aa2667a305329661e5a7a0bd052b9f4f5b3fe6
r-pr <rus.prokope@gmail.com>
2019-03-01 13:32:41 +0200

    add monokai theme

--- config.php
+++ config.php
@@ -1,19 +1,22 @@
 <?php
 
 class Config {
+	public static $siteTitle = 'My Blog';
+
     // you may also set EBE_ADMIN_PWD environment variable.
     // ebe will first look for it, if not set will use this config property
     public static $adminPassword = '111';
-    public static $siteTitle = 'My Blog';
+
     // db settings
-    //public static $dbEngine = 'mysql';
-    public static $dbEngine = 'sqlite';
+    public static $dbEngine = 'sqlite';	// or 'mysql'
+    // only for mysql:
     public static $dbHost = 'localhost';
     public static $dbName = 'ebe';
     public static $dbUser = 'root';
     public static $dbPassw = '';
-    //
-    public static $theme = 'default';
+
+    public static $theme = 'monokai';	// or 'default'
+    
     // number of symbols of post body in preview
     public static $maxTextInPreview = 1000;
 }
